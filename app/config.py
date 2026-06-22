6eff124fa6fbea40c3ae810c6a24488cdf838688
Nicolas Zambrano <nizarizambrano@hotmail.com>
2026-06-22 16:49:50 -0500

    Correcciones

--- app/config.py
+++ app/config.py
@@ -1,13 +1,12 @@
 import os
-from dotenv import load_dotenv 1
+from dotenv import load_dotenv 
 
 load_dotenv()
 
 class Config:
-    SECRET_KEY = OS.GETENV('SECRET_KEY','dev-secret')
-    # COnfiguracion de la BD
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
+    # Configuración de la BD
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://(os.getenv('DB_USER')):(os.getenv('DB_PASSWORD'))"
-        f"@(os.getenv('DB_HOST'))/(os.getenv('DB_NAME'))"
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
