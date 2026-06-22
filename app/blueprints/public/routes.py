a540cef1fa155fa3ac00bdd92b4a98b2d9ead3e4
Nicolas Zambrano <nizarizambrano@hotmail.com>
2026-06-22 15:41:43 -0500

    Configurando el auth

--- app/blueprints/public/routes.py
+++ app/blueprints/public/routes.py
@@ -1,14 +1,14 @@
 from flask import render_template
 from . import public_bp
 
-@public_bp.route('/') # <--- Corregido: con comillas
+@public_bp.route('/') 
 def home():
     return render_template('public/home.html')
 
-@public_bp.route('/tienda') # <--- Corregido: con slash inicial
+@public_bp.route('/tienda') 
 def tienda():
     return render_template('public/tienda.html')
 
-@public_bp.route('/contacto') # <--- Corregido: con slash inicial
+@public_bp.route('/contacto') 
 def contacto():
     return render_template('public/contacto.html')
